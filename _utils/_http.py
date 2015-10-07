ac2283d6bd837c12189503cac9c6309c9c412ce1
Freedom <Vietworm@users.noreply.github.com>
2015-10-07 20:15:33 +0700

    Add function, check must be URL isFile or whatever

--- _utils/_http.py
+++ _utils/_http.py
@@ -2,6 +2,7 @@ import os
 import requests
 import urllib
 import urllib2
+import mimetypes
 
 
 def download(url, pathSave):
@@ -12,3 +13,10 @@ def get_redirected_url(url):
     opener = urllib2.build_opener(urllib2.HTTPRedirectHandler)
     request = opener.open(url)
     return request.url
+    
+def isFile(url):
+	"""Return boolean, using check must be URL isFile or whatever"""
+	if mimetypes.guess_type(url, strict=True)[0] == None:
+		return False
+	else:
+		return True
